d8c21bebb4d8137b7c08c6014b1920e7247715c2
halder057 <halder.cse.kuet@hotmail.com>
2016-09-05 13:44:07 +0600

    test files

---
+++ BootstrapTest/BootstrapTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapTest.Startup))]
+namespace BootstrapTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
